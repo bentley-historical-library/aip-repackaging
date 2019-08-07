d959a5fb6b3a73650902e0adcf27d5b6552e6812
Dallas <djpillen@umich.edu>
2019-08-07 10:14:24 -0400

    add README and setup scripts

---
+++ README.md
@@ -0,0 +1,10 @@
+# BHL AIP Repackager
+Repackages AIPs for the Bentley Historical Library's Archivematica --> DSpace workflow
+
+## Installation
+`pip install git+https://github.com/djpillen/aip-repackaging.git`
+
+## Use
+This package installs a script, `aip_repacker.py`, that can be used to copy AIPs from Archivematia's AIP Storage into a working directory, split and repackage the AIPs into `metadata.zip` and `objects.zip`, upload the AIPs to DSpace, and update ArchivesSpace with digital objects.
+
+The use of this tool is highly localized to practices at the BHL.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+requirements.append("DAPPr")
+requirements.append("bhlaspaceapiclient")
+
+setup(
+    name='aip-repackaging',
+    version='0.0.1',
+    url='https://github.com/djpillen/aip-repackaging',
+    author='Dallas Pillen',
+    packages=find_packages(),
+    scripts=['aip_repackager.py'],
+    description='Bentley Historical Library scripts to repackage Archivematica AIPs',
+    dependency_links=['git+https://github.com/bentley-historical-library/DAPPr.git#egg=DAPPr', 'git+https://github.com/bentley-historical-library/bhlaspaceapiclient.git#egg=bhlaspaceapiclient'],
+    install_requires=requirements
+)
