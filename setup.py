36e3293962a7b13770976dc4f4e63dbb83065948
Dallas <djpillen@umich.edu>
2019-08-08 13:04:02 -0400

    add versions to dependency_links

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     packages=find_packages(),
     scripts=['aip_repackager.py'],
     description='Bentley Historical Library scripts to repackage Archivematica AIPs',
-    dependency_links=['git+https://github.com/bentley-historical-library/DAPPr.git#egg=DAPPr', 'git+https://github.com/bentley-historical-library/bhlaspaceapiclient.git#egg=bhlaspaceapiclient'],
+    dependency_links=['git+https://github.com/bentley-historical-library/DAPPr.git#egg=DAPPr-0.0.1', 'git+https://github.com/bentley-historical-library/bhlaspaceapiclient.git#egg=bhlaspaceapiclient-0.0.1'],
     install_requires=requirements
 )
