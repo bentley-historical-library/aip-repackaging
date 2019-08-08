09e1c4219655af6f98b5d6a4cc1bb4825e513a0a
Dallas <djpillen@umich.edu>
2019-08-08 13:16:56 -0400

    add versions to requirements

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-requirements.append("DAPPr")
-requirements.append("bhlaspaceapiclient")
+requirements.append("DAPPr=0.0.1")
+requirements.append("bhlaspaceapiclient=0.0.1")
 
 setup(
     name='aip-repackaging',
@@ -14,6 +14,9 @@ setup(
     packages=find_packages(),
     scripts=['aip_repackager.py'],
     description='Bentley Historical Library scripts to repackage Archivematica AIPs',
-    dependency_links=['git+https://github.com/bentley-historical-library/DAPPr#egg=DAPPr-0.0.1', 'git+https://github.com/bentley-historical-library/bhlaspaceapiclient#egg=bhlaspaceapiclient-0.0.1'],
+    dependency_links=[
+                    'git+https://github.com/bentley-historical-library/DAPPr.git@master#egg=DAPPr-0.0.1',
+                    'git+https://github.com/bentley-historical-library/bhlaspaceapiclient.git@master#egg=bhlaspaceapiclient-0.0.1'
+                    ],
     install_requires=requirements
 )
